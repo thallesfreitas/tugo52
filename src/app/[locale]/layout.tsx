48be3f60ead3669d94e1d76af23ff71a4da232bc
Thalles Freitas <thallesfreitas@yahoo.com.br>
2025-01-15 11:25:23 -0300

    Tugo front - added GA

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 "use server";
-import { NextIntlClientProvider, useLocale } from "next-intl";
+import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,12 @@
 import Home from "./home";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 export default async function LocaleLayout() {
-	return <Home />;
+  return (
+    <>
+      <Home />
+
+      <GoogleAnalytics gaId={process.env.GA ?? ""} />
+    </>
+  );
 }

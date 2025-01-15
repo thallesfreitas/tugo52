68d1d356b2cc2a579a34beada2a277c5e3ffc475
Thalles Freitas <thallesfreitas@yahoo.com.br>
2025-01-15 19:04:20 -0300

    update storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { NextIntlClientProvider } from "next-intl";
 import type { Preview } from "@storybook/react";
-import defaultMessages from "../src/locales/pt.json";
+import defaultMessages from "../messages/en.json";
 
 const preview: Preview = {
-	decorators: [
-		(Story) => (
-			<NextIntlClientProvider locale="pt" messages={defaultMessages}>
-				<Story />
-			</NextIntlClientProvider>
-		),
-	],
+  decorators: [
+    (Story) => (
+      <NextIntlClientProvider locale="pt" messages={defaultMessages}>
+        <Story />
+      </NextIntlClientProvider>
+    ),
+  ],
 };
 
 export default preview;
